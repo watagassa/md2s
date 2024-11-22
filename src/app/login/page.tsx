e2103ea095286109ef003f3a8bb09bdd801f2cad
watagassa <135955629+watagassa@users.noreply.github.com>
2024-11-22 20:22:36 +0900

    update:ログイン画面完成

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,25 @@
 
 import React from "react";
 import { useEffect } from "react";
-
 import { redirect } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import { Box, Button, FormControl } from "@yamada-ui/react";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  Center,
+  Container,
+  Flex,
+  Icon,
+  InputLeftElement,
+  Spacer,
+  Stack,
+  VStack,
+} from "@yamada-ui/react";
+import { FcGoogle } from "react-icons/fc";
+import { SiQiita } from "react-icons/si";
 
 const LoginPage = () => {
   const { data: session, status } = useSession();
@@ -26,19 +40,33 @@ const LoginPage = () => {
       redirect("/");
     }
   };
-
   return (
-    <Box>
-      <FormControl>
-        <Button
-          colorScheme="primary"
-          onClick={handleLogin("google")}
-          type="button"
-        >
-          Googleでログイン
-        </Button>
-      </FormControl>
-    </Box>
+    <Center p="4xl">
+      <Card p="md" gap="md">
+        <Center>ログイン</Center>
+        <Flex>
+          <ButtonGroup variant="outline" w={"full"}>
+            <Button onClick={handleLogin("google")} type="button" w={"full"}>
+              <Icon as={FcGoogle} h="80%" w="10" fontSize="full"></Icon>
+              <Spacer />
+              <Center>Googleでログイン</Center>
+              <Spacer />
+            </Button>
+          </ButtonGroup>
+        </Flex>
+
+        <Flex>
+          <ButtonGroup variant="outline" w={"full"}>
+            <Button onClick={handleLogin("google")} type="button" w={"full"}>
+              <Icon as={SiQiita} h="80%" w="10" fontSize="full"></Icon>
+              <Spacer />
+              <Center>Qiitaでログイン(実装中)</Center>
+              <Spacer />
+            </Button>
+          </ButtonGroup>
+        </Flex>
+      </Card>
+    </Center>
   );
 };
 
