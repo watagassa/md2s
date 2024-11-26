44abb9bc76b0866a422ee37c690e07f0c5b038b2
watagassa <135955629+watagassa@users.noreply.github.com>
2024-11-26 14:56:01 +0900

    add:marpスライド作成

--- src/app/posts/post_id/page.tsx
+++ src/app/posts/post_id/page.tsx
@@ -1,19 +1,33 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import MdSlideToggle from "@/app/_components/mdToSlide/MdSlideToggle";
-import { Box } from "@yamada-ui/react";
+import { Box, Flex, Spacer } from "@yamada-ui/react";
 import MarkdownPreview from "@/app/_components/markdown/MarkdownPreview";
-const page = () => {
-  // const post = testPostData;
+import { testPostData } from "@/app/_testData";
+import SlidePreview from "@/app/_components/slide/SlidePreview";
+const PostView = () => {
+  // 仮置きデータ
+  const post = testPostData;
+
+  const [isMarkdownView, setIsMarkdownView] = useState(true);
   return (
-    <Box>
-      <div>post_idページ</div>
-      <MdSlideToggle></MdSlideToggle>
-      <Box>
-        <MarkdownPreview></MarkdownPreview>
-      </Box>
+    <Box p={"lg"}>
+      <Flex>
+        <Spacer />
+        <MdSlideToggle
+          isMarkdownView={isMarkdownView}
+          setIsMarkdownView={setIsMarkdownView}
+        />
+      </Flex>
+      {isMarkdownView ? (
+        <Box p={"md"}>
+          <MarkdownPreview md={post.md} />
+        </Box>
+      ) : (
+        <SlidePreview slide={post.marp} />
+      )}
     </Box>
   );
 };
 
-export default page;
+export default PostView;
