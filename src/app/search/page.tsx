683a0dcc510e11e47ae890c8e68754496f231cb3
ogasa897 <135966582+ogasa897@users.noreply.github.com>
2024-11-28 20:40:56 +0900

    fix: /searchの検索バーの背景を枠に収まるよう修正

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,25 +1,38 @@
+"use client";
 import React from "react";
 import { testPostData2 } from "@/app/_testData";
 import { PostCard } from "@/app/_components/postCard/postCard";
-import { Box, VStack, Text, Flex, Input, InputGroup, InputLeftElement } from "@yamada-ui/react";
+import {
+  Box,
+  VStack,
+  Text,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Card
+} from "@yamada-ui/react";
 import { SearchIcon } from "@yamada-ui/lucide";
 
-const page = () => {
+export default function SearchPost() {
   const posts = testPostData2; //取得したユーザーが投稿した記事データ
 
   return (
     <Box bgColor={"blackAlpha.50"} py={"xl"}>
       <Flex justify="center">
-        <Text w="max(50%, sm)" paddingInline={"md"} paddingBottom={"sm"}>記事を検索</Text>
+        <Text w="max(50%, sm)" paddingInline={"md"} paddingBottom={"sm"}>
+          記事を検索
+        </Text>
       </Flex>
       <Flex justify="center" paddingBottom={"lg"}>
-        <InputGroup bgColor={"whiteAlpha.950"} w="max(50%, sm)">
-          <InputLeftElement>
-            <SearchIcon color={"indigo.700"} />
-          </InputLeftElement>
-          {/* TODO:以下のplaceholder修正した方がいいと思おう */}
-          <Input type="search" placeholder="     search" />
-        </InputGroup>
+        <Card bgColor={"whiteAlpha.950"} w="max(50%, sm)">
+          <InputGroup>
+            <InputLeftElement>
+              <SearchIcon color={"indigo.700"} />
+            </InputLeftElement>
+            <Input type="search" placeholder="search" />
+          </InputGroup>
+        </Card>
       </Flex>
       <VStack gap={"md"}>
         {posts.map((post) => (
@@ -28,6 +41,4 @@ const page = () => {
       </VStack>
     </Box>
   );
-};
-
-export default page;
+}
