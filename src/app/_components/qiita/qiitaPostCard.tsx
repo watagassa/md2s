d93c5dbed4007ab4e7c4183fcb16845067c0db6e
watagassa <135955629+watagassa@users.noreply.github.com>
2025-01-17 01:32:50 +0900

    add:Qiitaの記事を取得して表示する

---
+++ src/app/_components/qiita/qiitaPostCard.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { v4 as uuidv4 } from "uuid";
+import {
+  Avatar,
+  Card,
+  CardFooter,
+  CardHeader,
+  Flex,
+  Spacer,
+  Box,
+  Text,
+  Tag,
+} from "@yamada-ui/react";
+import { HeartIcon } from "@yamada-ui/lucide";
+import { type Article } from "@/types/post";
+import PostTitle from "../postCard/postTitle";
+import QiitaPostTitle from "./qiitaPostTitle";
+
+interface CardProps {
+  isQiita: boolean;
+  post: Article;
+}
+
+export function QiitaPostCard({ isQiita, post }: CardProps) {
+  const created_date: Date = new Date(post.created_at);
+  const date: string =
+    created_date.getUTCFullYear() +
+    "年" +
+    (created_date.getUTCMonth() + 1) +
+    "月" +
+    created_date.getUTCDate() +
+    "日";
+  return (
+    <Flex justify="center">
+      <Card
+        bgColor={"whiteAlpha.950"}
+        w="max(70%, sm)"
+        rounded="4"
+        paddingInline={"md"}
+      >
+        <CardHeader>
+          <Avatar size="sm" name={post.name} src={post.icon_url} />
+          <Box>
+            <Text>{post.name}</Text>
+            <Text>{date}</Text>
+          </Box>
+        </CardHeader>
+        {isQiita ? (
+          <>
+            <QiitaPostTitle post={post} />
+          </>
+        ) : (
+          <PostTitle id={post.id} title={post.title} />
+        )}
+        <CardFooter>
+          <Flex gap="md">
+            {post.tags?.map((tag) => (
+              <Tag
+                key={uuidv4()}
+                paddingInline="sm"
+                bgColor={"neutral.50"}
+                color={"black"}
+              >
+                {tag.word}
+              </Tag>
+            ))}
+          </Flex>
+          <Spacer />
+          <Flex align="center">
+            <HeartIcon alignItems="none" />
+            <Text p="xs">{post.like_count}</Text>
+          </Flex>
+        </CardFooter>
+      </Card>
+    </Flex>
+  );
+}
