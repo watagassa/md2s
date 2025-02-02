af205ea754f5a19844580bc226e83bba4a84e211
ogasa897 <135966582+ogasa897@users.noreply.github.com>
2025-02-02 21:26:25 +0900

    refactor:  userページのlogicとviewのファイル分け
    - userページのviewとlogicをファイル分けした
    - userページのAPIから取得するuserの合計いいね数と合計投稿記事数のAPI側の変数名が変更されていたので、それに合わせて変更

---
+++ src/app/user/logic/Logic.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Article } from "@/types/post";
+import {
+  Center,
+  Loading,
+} from "@yamada-ui/react";
+import { useAtom } from "jotai";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getUserArticles } from "../../api/article/article";
+import { userAtom } from "../../atoms/atom";
+import Page from "../view";
+
+export default function PostPageLogic() {
+  const { status } = useSession();
+  const [posts, setPosts] = useState<Article[] | null>(null);
+  const [user] = useAtom(userAtom);
+
+  useEffect(() => {
+    if (status === "authenticated" && user) {
+      getUserArticles(user.id).then((data) => setPosts(data));
+    }
+  }, [status, user]);
+
+  if (status === "loading") {
+    return (
+      <Center height="100vh">
+        <Loading variant="oval" fontSize="8xl" color="indigo.700" />
+      </Center>
+    );
+  }
+
+  if (status !== "authenticated") {
+    redirect("/login");
+    return null;
+  }
+
+  return (
+    <Page
+     posts = {posts}
+     user = {user}
+    />
+  );
+}
