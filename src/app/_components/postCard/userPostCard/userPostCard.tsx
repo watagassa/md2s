8186a9c3befd5803f0a0a9b70aea5fe95d639848
watagassa <135955629+watagassa@users.noreply.github.com>
2025-02-03 14:19:22 +0900

    fix:postcardで同じタグがあるときのid重複エラーを解消
    add:user画面用のuserpostcard作成
    記事編集画面と記事登録画面に遷移できる
    
    add:articleのAPIにDELETEを追加

---
+++ src/app/_components/postCard/userPostCard/userPostCard.tsx
@@ -0,0 +1,43 @@
+import { type Article } from "@/types/post";
+import { UserPostCardView } from "./userPostCardView";
+import { redirect } from "next/navigation";
+import { useDisclosure } from "@yamada-ui/react";
+import { deleteArticle } from "@/app/api/article/article";
+import { userSessionAtom } from "@/app/atoms/atom";
+import { useAtom } from "jotai";
+interface UserCardProps {
+  post: Article;
+}
+
+export function UserPostCard({ post }: UserCardProps) {
+  const created_date: Date = new Date(post.created_at);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [userSession] = useAtom(userSessionAtom);
+  const date: string =
+    created_date.getUTCFullYear() +
+    "年" +
+    (created_date.getUTCMonth() + 1) +
+    "月" +
+    created_date.getUTCDate() +
+    "日";
+  const handleEdit = () => {
+    redirect(`posts/edit/${post.id}`);
+  };
+  const handleDelete = () => {
+    if (userSession?.idToken) {
+      deleteArticle(userSession.idToken, post.id);
+    }
+  };
+
+  return (
+    <UserPostCardView
+      post={post}
+      date={date}
+      isOpen={isOpen}
+      onOpen={onOpen}
+      onClose={onClose}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    ></UserPostCardView>
+  );
+}
